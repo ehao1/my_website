376f9a2ee7299bc9306d68a0f05dc0726e74d9f4
Eric Hao <ehao@berkeley.edu>
2020-10-03 12:16:09 -0400

    fake nav bar

--- src/App.js
+++ src/App.js
@@ -2,54 +2,32 @@ import React from 'react';
 import logo from './CoFED_logo.png';
 import './App.css';
 
-
-function leftRight(){
-  return (
-    <div>
-
-      <div class="flex-container">
-        <div class = "box">
-          <h1>Bruh</h1>
-        </div>
-      </div>
-
-      <div className="leftRight-container">
-          
-          <div className="text-container">
-          <hr/>
-          <h1 className="header">Projects</h1>
-          <h2 className="sub-header"> CoFED web application (2020) </h2>
-            <div className="text-item">
-            <p>
-              CoFED (Cooperative Food Empowerment Directive) is a non-profit 
-              organization that partners with young folks of color to build
-              food and land co-ops.
-            </p>
-            <p>
-            I’m currently working with a wonderful team and CoFed
-            to build a web portal for co-ops to connect with each other.
-            </p>
-            </div>
-          </div>
-
-          <img src={logo} className="image-item" alt="logo" />
-      </div>
-
-    </div>
-  );
-}
 //things to do
 //1) nav bar
-//2) background images
 //3) organize files
 //4) make the fonts and style look better
-
+ 
 function App() {
   return (
     <div>
+      <div class="navbar">
+          <u1 class="menu-area">
+            <li><a href="#"> Home</a></li>
+            <li><a> /</a></li>
+            <li><a href="#"> About</a></li>
+            <li><a> /</a></li>
+            <li><a href="#"> Experinces</a></li>
+            <li><a> /</a></li>
+            <li><a href="#"> Projects</a></li>
+            <li><a> /</a></li>
+            <li><endItem href="#"> Contacts</endItem></li>
+   
 
-      <div className="intro-box">
-        <div className="box">
+          </u1>
+      </div>
+
+      <div className="intro-page">
+        <div className="intro-box">
           <h3>Hey, I'm</h3>
           <h1>Eric Hao</h1>
         </div>
