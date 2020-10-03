45cd9e0d63b8edade151dd4a059fb75bcff3e618
Eric Hao <ehao@berkeley.edu>
2020-10-03 02:49:40 -0400

    homepage layout + basic page layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './CoFED_logo.png';
 import './App.css';
+
+
+function leftRight(){
+  return (
+    <div>
+
+      <div class="flex-container">
+        <div class = "box">
+          <h1>Bruh</h1>
+        </div>
+      </div>
+
+      <div className="leftRight-container">
+          
+          <div className="text-container">
+          <hr/>
+          <h1 className="header">Projects</h1>
+          <h2 className="sub-header"> CoFED web application (2020) </h2>
+            <div className="text-item">
+            <p>
+              CoFED (Cooperative Food Empowerment Directive) is a non-profit 
+              organization that partners with young folks of color to build
+              food and land co-ops.
+            </p>
+            <p>
+            I’m currently working with a wonderful team and CoFed
+            to build a web portal for co-ops to connect with each other.
+            </p>
+            </div>
+          </div>
+
+          <img src={logo} className="image-item" alt="logo" />
+      </div>
+
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className="intro-box">
+        <div className="box">
+          <h3>Hey, I'm</h3>
+          <h1>Eric Hao</h1>
+        </div>
+      </div>
+
+      <div className="leftRight-container">
+          
+          <div className="text-container">
+          <hr/>
+          <h1 className="header">Projects</h1>
+          <h2 className="sub-header"> CoFED web application (2020) </h2>
+            <div className="text-item">
+            <p>
+              CoFED (Cooperative Food Empowerment Directive) is a non-profit 
+              organization that partners with young folks of color to build
+              food and land co-ops.
+            </p>
+            <p>
+            I’m currently working with a wonderful team and CoFed
+            to build a web portal for co-ops to connect with each other.
+            </p>
+            </div>
+          </div>
+
+          <img src={logo} className="image-item" alt="logo" />
+      </div>
+
     </div>
+
+
+
+    /* // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div> */
   );
 }
 
