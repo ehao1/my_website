f3c312f2cdf5b8bd4f0d0812fe26cb46b6b7e128
Eric Hao <ehao@berkeley.edu>
2020-11-27 21:38:16 -0500

    fixed typos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React from 'react';
+import React from "react";
 // import logo from './images/CoFED_logo.png';
 // import rlImage from './images/rl-image.png';
 // import Geophysics from './images/Geophysics.png';
-import './App.css';
-import ProjFunc from './ProjFunc.js';
-import axios from 'axios';
-import AboutFunc from './AboutFunc';
-
+import "./App.css";
+import ProjFunc from "./ProjFunc.js";
+import axios from "axios";
+import AboutFunc from "./AboutFunc";
 
 //do the enviornment key stuff
 //integrate images
 
 //update about
 
-
 function App() {
-
   const [about, setAbout] = React.useState(null);
   const [projects, setProjects] = React.useState(null);
-  const [experinces, setExperinces] = React.useState(null);
+  const [experiences, setExperiences] = React.useState(null);
   const [contact, setContact] = React.useState(null);
-  
+
   async function fetchData() {
     const responseProject = await axios.get(
-      'https://api.airtable.com/v0/appUYJORSDB0bukw7/ProjectList',
+      "https://api.airtable.com/v0/appUYJORSDB0bukw7/ProjectList",
       {
         headers: {
           authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
@@ -31,8 +28,8 @@ function App() {
       }
     );
 
-    const responseExperince = await axios.get(
-      'https://api.airtable.com/v0/appUYJORSDB0bukw7/ExperincesList',
+    const responseExperience = await axios.get(
+      "https://api.airtable.com/v0/appUYJORSDB0bukw7/ExperiencesList",
       {
         headers: {
           authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
@@ -41,7 +38,7 @@ function App() {
     );
 
     const responseContact = await axios.get(
-      'https://api.airtable.com/v0/appUYJORSDB0bukw7/ContactList',
+      "https://api.airtable.com/v0/appUYJORSDB0bukw7/ContactList",
       {
         headers: {
           authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
@@ -50,7 +47,7 @@ function App() {
     );
 
     const responseAbout = await axios.get(
-      'https://api.airtable.com/v0/appUYJORSDB0bukw7/AboutList',
+      "https://api.airtable.com/v0/appUYJORSDB0bukw7/AboutList",
       {
         headers: {
           authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
@@ -60,19 +57,17 @@ function App() {
 
     setAbout(responseAbout.data.records);
     setProjects(responseProject.data.records);
-    setExperinces(responseExperince.data.records);
+    setExperiences(responseExperience.data.records);
     setContact(responseContact.data.records);
-
   }
 
-  
   React.useEffect(() => {
     fetchData();
   }, []);
-  
-  if (!projects || !experinces || !contact || !about) {
+
+  if (!projects || !experiences || !contact || !about) {
     return (
-        <div className="intro-page" id="home">
+      <div className="intro-page" id="home">
         <div className="intro-box">
           <h3>please wait</h3>
           <h1>loading...</h1>
@@ -81,26 +76,45 @@ function App() {
     );
   }
 
-
   return (
     //nav bar
     <div>
       <div class="navbar">
-          <u1 class="menu-area">
-            <li><a href="#home"> (--  Home</a></li>
-            <li><a> /</a></li>
-            <li><a href="#about"> About</a></li>
-            <li><a> /</a></li>
-            <li><a href="#experinces"> Experinces</a></li>
-            <li><a> /</a></li>
-            <li><a href="#projects"> Projects</a></li>
-            <li><a> /</a></li>
-            <li><a href="#contact"> Contact</a></li>
-            <li><endItem> --)</endItem></li>
-          </u1>
+        <u1 class="menu-area">
+          <li>
+            <a href="#home"> (-- Home</a>
+          </li>
+          <li>
+            <a> /</a>
+          </li>
+          <li>
+            <a href="#about"> About</a>
+          </li>
+          <li>
+            <a> /</a>
+          </li>
+          <li>
+            <a href="#experiences"> Experiences</a>
+          </li>
+          <li>
+            <a> /</a>
+          </li>
+          <li>
+            <a href="#projects"> Projects</a>
+          </li>
+          <li>
+            <a> /</a>
+          </li>
+          <li>
+            <a href="#contact"> Contact</a>
+          </li>
+          <li>
+            <endItem> --)</endItem>
+          </li>
+        </u1>
       </div>
 
-    {/*Intro page */}
+      {/*Intro page */}
       <div className="intro-page" id="home">
         <div className="intro-box">
           <h3>Hey, I'm</h3>
@@ -108,66 +122,56 @@ function App() {
         </div>
       </div>
 
-    {/*About page */}
+      {/*About page */}
       <div className="leftRight-container-left" id="about">
-          
-          <div className="text-container-right-border">
+        <div className="text-container-right-border">
           <h1 className="header">About</h1>
-            {about.map((record) => (
-            <AboutFunc key={record.id} info={record.fields} />))
-            }
-          </div>
+          {about.map((record) => (
+            <AboutFunc key={record.id} info={record.fields} />
+          ))}
+        </div>
       </div>
 
-    {/*Experinces page */}
-    <div className="leftRight-container-right" id="experinces">
-          
-          <div>
+      {/*Experiences page */}
+      <div className="leftRight-container-right" id="experiences">
+        <div>
           {/* <img src={rlImage} className="image-item" alt="rlImage" />
           <img src={Geophysics} className="image-item" alt="Geophysics" /> */}
-          </div>
+        </div>
 
-          <div className="text-container">
-          <h1 className="header">Experinces</h1>
-          {experinces.map((record) => (
-              <ProjFunc key={record.id} info={record.fields} />
-            ))}
-          </div>
+        <div className="text-container">
+          <h1 className="header">Experiences</h1>
+          {experiences.map((record) => (
+            <ProjFunc key={record.id} info={record.fields} />
+          ))}
+        </div>
       </div>
-    
 
-    {/*Projects page */}
+      {/*Projects page */}
       <div className="leftRight-container-left" id="projects">
-          <div className="text-container">
+        <div className="text-container">
           <h1 className="header">Projects</h1>
           <div>
-          {projects.map((record) => (
+            {projects.map((record) => (
               <ProjFunc key={record.id} info={record.fields} />
             ))}
           </div>
-          </div>
-          {/* <img src={logo} className="image-item" alt="logo" /> */}
+        </div>
+        {/* <img src={logo} className="image-item" alt="logo" /> */}
       </div>
 
-
-   {/*Contact */}
-   <div className="leftRight-container-right" id="contact">
-
-          <div className="text-container-contact">
+      {/*Contact */}
+      <div className="leftRight-container-right" id="contact">
+        <div className="text-container-contact">
           <h1 className="header">Contact</h1>
           <div>
-          {contact.map((record) => (
+            {contact.map((record) => (
               <ProjFunc key={record.id} info={record.fields} />
             ))}
           </div>
-          </div>
-          
+        </div>
       </div>
-
-
-
     </div>
-
   );
 }
 
--- src/vc/original.js
+++ src/vc/original.js
@@ -1,34 +1,120 @@
-import React from 'react';
-import logo from './images/CoFED_logo.png';
-import rlImage from './images/rl-image.png';
-import Geophysics from './images/Geophysics.png';
-import './App.css';
+import React from "react";
+// import logo from './images/CoFED_logo.png';
+// import rlImage from './images/rl-image.png';
+// import Geophysics from './images/Geophysics.png';
+import "./App.css";
+import ProjFunc from "./ProjFunc.js";
+import axios from "axios";
+import AboutFunc from "./AboutFunc";
 
-//things to do
-//1) nav bar
-//3) organize files
-//4) make the fonts and style look better
- 
+//do the enviornment key stuff
+//integrate images
+
+//update about
+
 function App() {
+  const [about, setAbout] = React.useState(null);
+  const [projects, setProjects] = React.useState(null);
+  const [experinces, setExperinces] = React.useState(null);
+  const [contact, setContact] = React.useState(null);
+
+  async function fetchData() {
+    const responseProject = await axios.get(
+      "https://api.airtable.com/v0/appUYJORSDB0bukw7/ProjectList",
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        },
+      }
+    );
+
+    const responseExperince = await axios.get(
+      "https://api.airtable.com/v0/appUYJORSDB0bukw7/ExperincesList",
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        },
+      }
+    );
+
+    const responseContact = await axios.get(
+      "https://api.airtable.com/v0/appUYJORSDB0bukw7/ContactList",
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        },
+      }
+    );
+
+    const responseAbout = await axios.get(
+      "https://api.airtable.com/v0/appUYJORSDB0bukw7/AboutList",
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        },
+      }
+    );
+
+    setAbout(responseAbout.data.records);
+    setProjects(responseProject.data.records);
+    setExperinces(responseExperince.data.records);
+    setContact(responseContact.data.records);
+  }
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!projects || !experinces || !contact || !about) {
+    return (
+      <div className="intro-page" id="home">
+        <div className="intro-box">
+          <h3>please wait</h3>
+          <h1>loading...</h1>
+        </div>
+      </div>
+    );
+  }
+
   return (
     //nav bar
     <div>
       <div class="navbar">
-          <u1 class="menu-area">
-            <li><a href="#home"> (--  Home</a></li>
-            <li><a> /</a></li>
-            <li><a href="#about"> About</a></li>
-            <li><a> /</a></li>
-            <li><a href="#experinces"> Experinces</a></li>
-            <li><a> /</a></li>
-            <li><a href="#projects"> Projects</a></li>
-            <li><a> /</a></li>
-            <li><a href="#contact"> Contact</a></li>
-            <li><endItem> --)</endItem></li>
-          </u1>
+        <u1 class="menu-area">
+          <li>
+            <a href="#home"> (-- Home</a>
+          </li>
+          <li>
+            <a> /</a>
+          </li>
+          <li>
+            <a href="#about"> About</a>
+          </li>
+          <li>
+            <a> /</a>
+          </li>
+          <li>
+            <a href="#experinces"> Experiences</a>
+          </li>
+          <li>
+            <a> /</a>
+          </li>
+          <li>
+            <a href="#projects"> Projects</a>
+          </li>
+          <li>
+            <a> /</a>
+          </li>
+          <li>
+            <a href="#contact"> Contact</a>
+          </li>
+          <li>
+            <endItem> --)</endItem>
+          </li>
+        </u1>
       </div>
 
-    {/*Intro page */}
+      {/*Intro page */}
       <div className="intro-page" id="home">
         <div className="intro-box">
           <h3>Hey, I'm</h3>
@@ -36,128 +122,56 @@ function App() {
         </div>
       </div>
 
-    {/*About page */}
+      {/*About page */}
       <div className="leftRight-container-left" id="about">
-          
-          <div className="text-container-right-border">
-          <hr/>
-          <h1 className="header-no-border">About</h1>
-            <div className="text-item">
-            <p>
-            I am currently a student at the University
-             of California Berkeley, intending to major
-              in computer science. 
-            </p>
-            <p>
-            I have always had a passion for STEM and love the thrill of learning.
-             I truly believe education is a beautfiul and generous thing. 
-            </p>
-            </div>
-          </div>
+        <div className="text-container-right-border">
+          <h1 className="header">About</h1>
+          {about.map((record) => (
+            <AboutFunc key={record.id} info={record.fields} />
+          ))}
+        </div>
       </div>
 
-    {/*Experinces page */}
-    <div className="leftRight-container-right" id="experinces">
-          
-          <div>
-          <img src={rlImage} className="image-item" alt="rlImage" />
-          <img src={Geophysics} className="image-item" alt="Geophysics" />
-          </div>
+      {/*Experinces page */}
+      <div className="leftRight-container-right" id="experinces">
+        <div>
+          {/* <img src={rlImage} className="image-item" alt="rlImage" />
+          <img src={Geophysics} className="image-item" alt="Geophysics" /> */}
+        </div>
 
-          <div className="text-container">
-          <h1 className="header">Experinces</h1>
-          <h2 className="sub-header"> Reinforment Learning Research (2019) </h2>
-            <div className="text-item">
-            <p>
-            Under the guidance of Dr. Gasarch from the University of Maryland, 
-            I used the Q-Learning algorithmn to train a machine to play 1, 2, 3 NIM. 
-            This research was purely explorative.
-            </p>
-            </div>
-
-            <h2 className="sub-header"> Geophysics Research (2018) </h2>
-              <div className="text-item">
-              <p>
-              I invested the phase changes of the water-methane-carbon dioxide
-              system using Raman Spectroscopy
-              at the Cargnegie Geophysics Lab. 
-              </p>
-              </div>
-          </div>
-          
-          
+        <div className="text-container">
+          <h1 className="header">Experiences</h1>
+          {experinces.map((record) => (
+            <ProjFunc key={record.id} info={record.fields} />
+          ))}
+        </div>
       </div>
 
-    {/*Projects page */}
+      {/*Projects page */}
       <div className="leftRight-container-left" id="projects">
-          
-          <div className="text-container">
-          <hr/>
+        <div className="text-container">
           <h1 className="header">Projects</h1>
-          <h2 className="sub-header"> CoFED web application (2020) </h2>
-            <div className="text-item">
-            <p>
-              CoFED (Cooperative Food Empowerment Directive) is a non-profit 
-              organization that partners with young folks of color to build
-              food and land co-ops.
-            </p>
-            <p>
-            I’m currently working with a wonderful team and CoFed
-            to build a web portal for co-ops to connect with each other.
-            </p>
-            </div>
+          <div>
+            {projects.map((record) => (
+              <ProjFunc key={record.id} info={record.fields} />
+            ))}
           </div>
-
-          <img src={logo} className="image-item" alt="logo" />
+        </div>
+        {/* <img src={logo} className="image-item" alt="logo" /> */}
       </div>
 
-
-   {/*Contact */}
-   <div className="leftRight-container-right" id="contact">
-
-          <div className="text-container-contact">
+      {/*Contact */}
+      <div className="leftRight-container-right" id="contact">
+        <div className="text-container-contact">
           <h1 className="header">Contact</h1>
-          <h2 className="sub-header"> Email </h2>
-            <div className="text-item">
-            <p>
-            ehao@berkeley.edu
-            </p>
-            </div>
-
-            <h2 className="sub-header"> Phone Number </h2>
-              <div className="text-item">
-              <p>
-              240 - 644 - 3738
-              </p>
-              </div>
+          <div>
+            {contact.map((record) => (
+              <ProjFunc key={record.id} info={record.fields} />
+            ))}
           </div>
-          
-          
+        </div>
       </div>
-
-
-
     </div>
-
-
-
-  // // <div className="App">
-  // //   <header className="App-header">
-  // //     <img src={logo} className="App-logo" alt="logo" />
-  // //     <p>
-  // //       Edit <code>src/App.js</code> and save to reload.
-  // //     </p>
-  // //     <a
-  // //       className="App-link"
-  // //       href="https://reactjs.org"
-  // //       target="_blank"
-  // //       rel="noopener noreferrer"
-  // //     >
-  // //       Learn React
-  // //     </a>
-  // //   </header>
-  // // </div> 
-  // );
   );
 }
 
